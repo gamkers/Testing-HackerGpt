d3b3f7fbf0a670e6e7e70e15774200dacb76eb00
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-02 17:11:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -166,14 +166,14 @@ if LOGGED_IN == True:
   .gradient-text {
       font-weight: bold;
       background: -webkit-linear-gradient(left, red, orange);
-      background: linear-gradient(to right, red,black);
+      background: linear-gradient(to right, red,orange);
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
       display: inline;
       font-size: 3em;
   }
   </style>
-  <div class="gradient-text">"Hay Hacker"</div>
+  <div class="gradient-text">Hay Hacker</div>
   """
   st.markdown(gradient_text_html, unsafe_allow_html=True)
   #st.markdown(gradient_text_html, unsafe_allow_html=True)
