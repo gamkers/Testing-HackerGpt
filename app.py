142219d1df0c7578efb21186ae9e285783fc97b7
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-02 15:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=st.secrets["gemini_api"])
 
 __login__obj = __login__(auth_token = "courier_auth_token",
                     company_name = "Shims",
-                    width = 200, height = 250,
+                    width = 300, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = False,
                     hide_footer_bool = False,
                     lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
