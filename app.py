a935ca74996716f955af7087f50a58ca9364aca7
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-02 16:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ __login__obj = __login__(auth_token = "courier_auth_token",
                     width = 400, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = False,
                     hide_footer_bool = False,
-                    lottie_url = 'https://lottie.host/9792acfe-c700-4d5a-a810-066e9f6ba012/wcPCApKiI6.json')
+                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
 LOGGED_IN= __login__obj.build_login_ui()
 username= __login__obj.get_username()
