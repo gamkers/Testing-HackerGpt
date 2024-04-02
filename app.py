3d0c18d463db6d81f9a4d331e76076f7031300b7
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-02 17:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -166,16 +166,16 @@ if LOGGED_IN == True:
   .gradient-text {
       font-weight: bold;
       background: -webkit-linear-gradient(left, red, orange);
-      background: linear-gradient(to right, red, orange);
+      background: linear-gradient(to right, red,black);
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
       display: inline;
       font-size: 3em;
   }
   </style>
-  <div class="gradient-text">GamkersGPT</div>
+  <div class="gradient-text">Hay Hacker!!/div>
   """
-  
+  st.markdown(gradient_text_html, unsafe_allow_html=True)
   #st.markdown(gradient_text_html, unsafe_allow_html=True)
   st.caption("AI Integrity: Ethical Hacking, Elevated - Developed by GAMKERS")
   model = st.radio(
